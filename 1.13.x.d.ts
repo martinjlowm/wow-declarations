051cc9c3c58e636316ecbea47d25ca2ee7def752
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-04-09 21:38:02 +0200

    Add 1.13.x declaration file with select imports

---
+++ 1.13.x.d.ts
@@ -0,0 +1,59 @@
+// global and constants
+import '@/constants';
+import '@/global';
+
+// A
+import '@/account';
+import '@/action';
+import '@/activity';
+import '@/addon';
+
+// B
+import '@/bank';
+import '@/battlefield';
+import '@/binding';
+import '@/buff';
+
+// C
+import '@/calendar';
+import '@/camera';
+import '@/channel';
+import '@/character';
+import '@/character-statistics';
+import '@/chat-info';
+import '@/chat-window';
+import '@/class';
+import '@/communication';
+import '@/companion';
+import '@/container';
+import '@/currency';
+import '@/cursor';
+
+// D
+import '@/debug';
+
+// E
+import '@/event';
+
+// G
+import '@/gossip';
+import '@/groups';
+
+// I
+import '@/inventory';
+
+// Q
+import '@/quest';
+
+// S
+import '@/security';
+import '@/spell';
+import '@/system';
+import '@/ui/tooltip';
+
+// U
+import '@/ui/ui';
+import '@/unit';
+
+export = WoWAPI;
+export as namespace WoWAPI;
