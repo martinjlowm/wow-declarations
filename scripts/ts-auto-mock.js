34f860a277e664a1057abdaa513a132b79d8b52a
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-06-17 21:34:11 +0200

    Switch to HTTPS for ts-auto-mock installation

--- scripts/ts-auto-mock.js
+++ scripts/ts-auto-mock.js
@@ -2,7 +2,7 @@ const pathToRepo = path.join(execEnv.tempDir, 'repo');
 const pathToArchive = path.join(execEnv.tempDir, 'archive.tgz');
 const pathToDistribution = path.join(pathToRepo, 'dist');
 
-child_process.execFileSync('git', ['clone', 'git@github.com:martinjlowm/ts-auto-mock.git', pathToRepo]);
+child_process.execFileSync('git', ['clone', 'https://github.com/martinjlowm/ts-auto-mock.git', pathToRepo]);
 
 child_process.execFileSync('git', ['checkout', 'release/v2.0.0-martinjlowm'], { cwd: pathToRepo });
 child_process.execFileSync('npm', ['ci'], { cwd: pathToRepo });
