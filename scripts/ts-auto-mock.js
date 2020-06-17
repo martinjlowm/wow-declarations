baccde28882b72b9129446a13826b3c444fe7361
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-06-17 20:57:19 +0200

    Add Yarn 2 patched build script for ts-auto-mock

---
+++ scripts/ts-auto-mock.js
@@ -0,0 +1,14 @@
+const pathToRepo = path.join(execEnv.tempDir, 'repo');
+const pathToArchive = path.join(execEnv.tempDir, 'archive.tgz');
+const pathToDistribution = path.join(pathToRepo, 'dist');
+
+child_process.execFileSync('git', ['clone', 'git@github.com:martinjlowm/ts-auto-mock.git', pathToRepo]);
+
+child_process.execFileSync('git', ['checkout', 'release/v2.0.0-martinjlowm'], { cwd: pathToRepo });
+child_process.execFileSync('npm', ['ci'], { cwd: pathToRepo });
+
+child_process.execFileSync('npm', ['run', 'build'], { cwd: pathToRepo });
+child_process.execFileSync('npm', ['run', 'package'], { cwd: pathToRepo });
+
+child_process.execFileSync('rmdir', [execEnv.buildDir]);
+child_process.execFileSync('mv', [pathToDistribution, execEnv.buildDir]);
