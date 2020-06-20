29238dcf3ae261956b7bb43ccea9ff23cb63ac6c
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2020-06-20 11:58:06 +0200

    Correct the ts-auto-mock checksum

--- scripts/ts-auto-mock.js
+++ scripts/ts-auto-mock.js
@@ -1,5 +1,6 @@
 const pathToRepo = path.join(execEnv.tempDir, 'repo');
 const pathToDistribution = path.join(pathToRepo, 'dist');
+const pathToArchive = path.join(execEnv.tempDir, 'archive.tgz');
 
 child_process.execFileSync('git', ['clone', 'https://github.com/martinjlowm/ts-auto-mock.git', pathToRepo]);
 
@@ -9,5 +10,9 @@ child_process.execFileSync('npm', ['ci'], { cwd: pathToRepo });
 child_process.execFileSync('npm', ['run', 'build'], { cwd: pathToRepo });
 child_process.execFileSync('npm', ['run', 'package'], { cwd: pathToRepo });
 
-child_process.execFileSync('rmdir', [execEnv.buildDir]);
-child_process.execFileSync('mv', [pathToDistribution, execEnv.buildDir]);
+// Delete the package.json such that it doesn't confuse Yarn thinking it's
+// related to the one in the dist directory
+child_process.execFileSync('rm', ['package.json'], { cwd: pathToRepo });
+
+child_process.execFileSync('yarn', ['pack', '--out', pathToArchive], { cwd: pathToDistribution });
+child_process.execFileSync('tar', ['-x', '-z', '--strip-components=1', '-f', pathToArchive, '-C', execEnv.buildDir]);
